92d42a276f48ae591c62a50b8be9b069fc41eb09
sina naghipour <style.npr@gmail.com>
2022-02-05 21:54:57 +0330

    insertion sort

---
+++ sort/insertion.py
@@ -0,0 +1,9 @@
+
+
+def insertion(arr):
+
+    for i in range(len(arr)):
+        for j in range(i):
+            if arr[j] > arr[i]:
+                arr[j], arr[i] = arr[i], arr[j]
+    return arr
---
+++ sort/main.py
@@ -0,0 +1,9 @@
+import insertion
+
+
+def main():
+    print(insertion.insertion([213,123,1,312,31,23,1]))
+
+
+if __name__ == "__main__" :
+    main()
